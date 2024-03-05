2187257a3bc6a35b6e23e2611ee010117b432b11
zxnd <billzand2025@gmail.com>
2024-03-04 22:29:09 -0500

    Added script.jss, index.html, and styles.css files

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript for form validation on Contact page
+function validateForm() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    if (name == '' || email == '' || message == '') {
+        alert('Please fill out all fields');
+        return false;
+    }
+}
